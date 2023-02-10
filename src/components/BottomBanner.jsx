d2f68609c7bb3c4b9597bceb7774e0ae47f98754
BerkantGC <berkant103x@hotmail.com>
2023-02-10 15:12:04 +0300

    bost-sell

--- src/components/BottomBanner.jsx
+++ src/components/BottomBanner.jsx
@@ -29,7 +29,7 @@ export default function()
                     <p style={{color: 'white', fontSize: '90%'}}>Şehirler bulvarı no.2c Bostanlı MH.  Karşıyaka İzmir</p>
                     <p style={{color: 'white', fontSize: '90%'}}>Telefon: +90 212 364 64 64<br></br>
                     Faks: +90 212 310 46 80<br/>
-                    E-mail: musteri.hizmetleri@arena.com.tr</p>
+                    E-mail: bost@bost-sell.com.tr</p>
                 </div>
             </div>
             <div className='all-links'>
